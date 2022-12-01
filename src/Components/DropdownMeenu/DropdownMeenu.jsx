9107188e2d24463eeb4b54ef941f262f192bb138
savinovaar@gmail.com <you@example.com>
2022-11-30 22:08:06 -0500

    changed contact me padge

--- src/Components/DropdownMeenu/DropdownMeenu.jsx
+++ src/Components/DropdownMeenu/DropdownMeenu.jsx
@@ -1,5 +1,6 @@
 
     import react, {useState} from "react";
+    import { Link } from 'react-router-dom';
 
 import './DropdownMeenu.css';
 
@@ -12,10 +13,16 @@ const DropdownMeenu = () =>{
         <div>
          <div className="DropdownMeenu">
         <div className="links-dropdown">
-           <a className="link-dropdown">HOME</a>
-           <a className="link-dropdown">ABOUT ME</a>
-           <a className="link-dropdown">MY PROJECTS</a>
-           <a className="link-dropdown">CONTACT ME</a>
+
+           <Link className='link-dropdown' to='/'>
+                  HOME
+             </Link>
+             <Link className='link-dropdown' to='/projects'>
+                  PROJECTS
+             </Link>
+             <Link className='link-dropdown' to='/contactme'>
+                  CONTACT ME
+             </Link>
         </div>
         </div>
        
